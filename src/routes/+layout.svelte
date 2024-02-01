f9189e4b0b55add502b4c5f1997bd1fd9ac41beb
dnk <edvard.all@gmail.com>
2024-02-01 19:27:36 +0100

    Rebuild in progress, completed navbar.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,18 @@
 <script>
- 	import Header from '$lib/components/Header.svelte'
-  import Footer from '$lib/components/Footer.svelte'
-  import '../app.css'
+  import "../app.css";
+
+  import NavBar from "$lib/NavBar.svelte"
+
+  let workExpRoutes = [
+    { text: "H&M GROUP", href: "/hmgroup" },
+    { text: "FANTASY", href: "/fantasy" },
+    { text: "COBINHOOD", href: "/cobinhood" },
+    { text: "THSR", href: "/thsr" },
+    { text: "MOTC x UMAJI", href: "/motcxu" },
+    { text: "RAKUTEN", href: "/rakuten" },
+  ]
 </script>
 
-<Header/>
-<slot></slot>
+<NavBar {workExpRoutes}/>
 
-<Footer id="contact"/>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,5 @@
-<script>
-	import ImageGrid from '$lib/components/ImageGrid.svelte';
-	import Carousel from '$lib/components/Carousel.svelte'
-
- 	import work from '$lib/data/work_data.json';
-</script>
-
-<div id="landing">
-	<Carousel/>
-	<div class="arrow-down">
-		<a href="#keynotes">
-			<img alt="scroll to next element" src="images/arrow-down.png"/>
-		</a>
-	</div>
-</div>
-
-<div id="keynotes">
-</div>
-
-<style>
-	#landing, #keynotes {
-		background-color: #FFF;
-		min-height: 100vh;
-	}
-
-	.arrow-down {
-	  display: flex;
-	  justify-content: center;
-	}
-</style>
+<!-- <main> 
+    <h1>
+        I CRAFT EXPERIENCE.
+    </h1>
+</main> -->
