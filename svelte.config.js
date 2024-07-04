45a8c2245b06af1167b2764be8df397d23c9d573
Edvard All <edvard.all@gmail.com>
2024-07-04 23:50:41 +0200

    trying new path base name

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -19,7 +21,7 @@ const config = {
 			precompress: false,
 			strict: true,
 			paths: {
-				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+				base: dev ? '' : "/imsyc-web"
 			}
 		})
 	}
