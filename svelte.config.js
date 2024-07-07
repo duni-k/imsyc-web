f82e5c29a2833f5473eae228870a9733443c59bc
Edvard All <edvard.all@gmail.com>
2024-07-07 11:17:39 +0200

    fuck github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -20,9 +18,6 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true,
-			paths: {
-				base: dev ? '' : "/imsyc-web"
-			}
 		})
 	}
 };
